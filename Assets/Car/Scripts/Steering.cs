4fc35014735d1d7ca157854221f0137e28371543
broaddaysk <omarbot@gmail.com>
2017-04-07 18:28:44 -0700

    first commit

---
+++ Assets/Car/Scripts/Steering.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour
+{
+    public float idealRPM = 500f;
+    public float maxRPM = 1000f;
+    public float AntiRoll = 200000.0f;
+
+    public Transform centerOfGravity;
+
+    public WheelCollider frontl; //the front left tire
+    public WheelCollider frontr; //the front right tire
+    public WheelCollider backl; //...so on...
+    public WheelCollider backr;
+
+    public float turnRadius = 30f; //the circle in which the tires can turn
+    public float torque = 2000f;
+    public float brakeTorque = 1000f;
+
+    public float wheel_mass = 120f;
+    public float wheel_spring = 20000f;
+    public float wheel_damper = 4000f;
+    public float wheel_extremum = 8f;
+
+    public float Speed()
+    {
+        return backr.radius * Mathf.PI * backr.rpm * .06f;
+    }
+
+    public float RPM()
+    {
+        return backl.rpm;
+    }
+
+    void DoRollBar(WheelCollider WheelL, WheelCollider WheelR)
+    {
+        WheelHit hit;
+        float travelL = 1.0f;
+        float travelR = 1.0f;
+
+        bool groundedL = WheelL.GetGroundHit(out hit);
+        if (groundedL)
+            travelL = (-WheelL.transform.InverseTransformPoint(hit.point).y - WheelL.radius) / WheelL.suspensionDistance;
+
+        bool groundedR = WheelR.GetGroundHit(out hit);
+        if (groundedR)
+            travelR = (-WheelR.transform.InverseTransformPoint(hit.point).y - WheelR.radius) / WheelR.suspensionDistance;
+
+        float antiRollForce = (travelL - travelR) * AntiRoll;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        frontl.mass = wheel_mass;
+        frontr.mass = wheel_mass;
+        backl.mass = wheel_mass;
+        backr.mass = wheel_mass;
+
+        frontl.suspensionSpring.spring.Equals(wheel_spring);
+        frontr.suspensionSpring.spring.Equals(wheel_spring);
+        backl.suspensionSpring.spring.Equals(wheel_spring);
+        backr.suspensionSpring.spring.Equals(wheel_spring);
+
+        frontl.suspensionSpring.damper.Equals(wheel_damper);
+        frontr.suspensionSpring.damper.Equals(wheel_damper);
+        backl.suspensionSpring.damper.Equals(wheel_damper);
+        backr.suspensionSpring.damper.Equals(wheel_damper);
+
+        frontl.sidewaysFriction.extremumValue.Equals(wheel_extremum);
+        frontr.sidewaysFriction.extremumValue.Equals(wheel_extremum);
+        backl.sidewaysFriction.extremumValue.Equals(wheel_extremum);
+        backr.sidewaysFriction.extremumValue.Equals(wheel_extremum);
+
+
+        //note: change spring to ~20000 and damper to ~4000 and extremum value to 4 or 5
+
+        this.GetComponent<Rigidbody>().centerOfMass = centerOfGravity.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float scaledTorque = -Input.GetAxis("Vertical") * torque;
+
+        if (backl.rpm < idealRPM) //1 torque
+            scaledTorque = Mathf.Lerp(scaledTorque / 10f, scaledTorque, backl.rpm / idealRPM);
+        else //max speed, therefore 0 torque
+            scaledTorque = Mathf.Lerp(scaledTorque, 0, (backl.rpm - idealRPM) / (maxRPM - idealRPM));
+
+        frontl.steerAngle = Input.GetAxis("Horizontal") * turnRadius;
+        frontr.steerAngle = Input.GetAxis("Horizontal") * turnRadius;
+
+        DoRollBar(frontr, frontl);
+        DoRollBar(backr, backl);
+
+        frontr.motorTorque = scaledTorque;
+        frontl.motorTorque = scaledTorque;
+        backr.motorTorque = scaledTorque;
+        backl.motorTorque = scaledTorque;
+
+        if(Input.GetKey(KeyCode.S)) //apply the brakes
+        {
+            frontr.brakeTorque = brakeTorque; //note: the first brake torque the wheel collider
+            frontl.brakeTorque = brakeTorque; //the second is a variable in this code
+            backr.brakeTorque = brakeTorque;
+            backl.brakeTorque = brakeTorque;
+        }
+        else //don't apply the brakes
+        {
+            frontr.brakeTorque = 0;
+            frontl.brakeTorque = 0;
+            backr.brakeTorque = 0;
+            backl.brakeTorque = 0;
+        }
+    }
+}
---
+++ Assets/Terrain/ModifyTerrain.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ModifyTerrain : MonoBehaviour
+{
+    public DepthWrapper dw;
+    public Terrain terr;
+    public float[,] heights;
+    public int smoothIterations = 1;
+    public float smoothBlend = 1.0f;
+    public int Tw;
+    public int Th;
+    public int neighbourhood = 0;
+    public int count = 0;
+    public float[,] tempHeights;
+    public float factor;
+
+    public float numSec = 1.0F;
+    public float startTime;
+
+    // Use this for initialization
+    void Start()
+    {
+        terr = Terrain.activeTerrain;
+        terr.terrainData.SetDetailResolution(1024, 8);
+        startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dw.pollDepth())
+        {
+            Tw = terr.terrainData.heightmapWidth;
+            Th = terr.terrainData.heightmapHeight;
+            heights = terr.terrainData.GetHeights(0, 0, Tw, Th);
+
+            //normalize, then extreme noise filter
+            for (int ii = 0; ii < 320 * 240; ii++)
+            {
+                //get x and y coords
+                int x = ii % 320;
+                int y = ii / 320;
+
+                float b = dw.depthImg[ii];
+                b = b / 7800;
+                b = 1 - b;
+                if (b > 0.7F)
+                {
+                    heights[y, x] = 0;
+                }
+                else
+                {
+                    heights[y, x] = b;
+                }
+            }
+            
+            //temporal filter, and set scene to modify terrain one every second
+            if (Time.time - startTime < numSec)
+            {
+                if (count == 0)
+                {
+                    tempHeights = (float[,])heights.Clone();
+                }
+                else
+                {
+                    for (int Ty = 0; Ty < Th; Ty++)
+                    {
+                        for (int Tx = 0; Tx < Tw; Tx++)
+                        {
+                            tempHeights[Tx, Ty] += heights[Tx, Ty];
+                        }
+                    }
+                }
+                count++;
+            }
+            else
+            {
+                factor = 1.0F / count;
+                for (int Ty = 0; Ty < Th; Ty++)
+                {
+                    for (int Tx = 0; Tx < Tw; Tx++)
+                    {
+                        tempHeights[Tx, Ty] *= factor;
+                    }
+                }
+                tempHeights = smooth(tempHeights, new Vector2(Tw, Th)); //smoothing filter applied
+                terr.terrainData.SetHeights(0, 0, tempHeights); //set the terrain to display the updated heightmap
+                count = 0;
+                startTime = Time.time;
+            }
+        }
+    }
+
+    //smoothing filter
+    private float[,] smooth(float[,] heightMap, Vector2 arraySize)
+    {
+        int Tw = (int)arraySize.x;
+        int Th = (int)arraySize.y;
+        int xNeighbours;
+        int yNeighbours;
+        int xShift;
+        int yShift;
+        int xIndex;
+        int yIndex;
+        int Tx;
+        int Ty;
+        // Start iterations...
+        for (int iter = 0; iter < smoothIterations; iter++)
+        {
+            for (Ty = 0; Ty < Th; Ty++)
+            {
+                // y...
+                if (Ty == 0)
+                {
+                    yNeighbours = 2;
+                    yShift = 0;
+                    yIndex = 0;
+                }
+                else if (Ty == Th - 1)
+                {
+                    yNeighbours = 2;
+                    yShift = -1;
+                    yIndex = 1;
+                }
+                else
+                {
+                    yNeighbours = 3;
+                    yShift = -1;
+                    yIndex = 1;
+                }
+                for (Tx = 0; Tx < Tw; Tx++)
+                {
+                    // x...
+                    if (Tx == 0)
+                    {
+                        xNeighbours = 2;
+                        xShift = 0;
+                        xIndex = 0;
+                    }
+                    else if (Tx == Tw - 1)
+                    {
+                        xNeighbours = 2;
+                        xShift = -1;
+                        xIndex = 1;
+                    }
+                    else
+                    {
+                        xNeighbours = 3;
+                        xShift = -1;
+                        xIndex = 1;
+                    }
+                    int Ny;
+                    int Nx;
+                    float hCumulative = 0.0f;
+                    int nNeighbours = 0;
+                    for (Ny = 0; Ny < yNeighbours; Ny++)
+                    {
+                        for (Nx = 0; Nx < xNeighbours; Nx++)
+                        {
+                            float heightAtPoint = heightMap[Tx + Nx + xShift, Ty + Ny + yShift]; // Get height at point
+                            hCumulative += heightAtPoint;
+                            nNeighbours++;
+                        }
+                    }
+                    float hAverage = hCumulative / nNeighbours;
+                    heightMap[Tx + xIndex + xShift, Ty + yIndex + yShift] = hAverage;
+                }
+            }
+        }
+        return heightMap;
+    }
+}
